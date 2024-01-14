43509863703163bc18cdbcb00c5a476d3251e9dd
unknown <hmansari2020@gimail.com>
2024-01-14 06:22:15 +0500

    Portfolio

---
+++ main.js
@@ -0,0 +1,19 @@
+var typed = new Typed('.text', {
+    strings: ['Frontend Devloper.', 'Backend Developer.', 'MERN Stack Developer.', 'Web Designer.'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+});
+
+function myFunction() {
+var x = document.getElementById("myTopnav");
+if (x.className === "topnav") {
+x.className += " responsive";
+} else {
+x.className = "topnav";
+};
+} ;
+
+
+
